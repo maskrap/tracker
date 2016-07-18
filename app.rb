081e93ecfdb0462892239c0883094cdc75f95c21
Matt Wilkin and Sam Park <sam@>
2016-07-18 15:25:35 -0700

    Pass rspec with migration and app filled

---
+++ app.rb
@@ -0,0 +1,46 @@
+require("sinatra/activerecord")
+require("sinatra")
+require("sinatra/reloader")
+also_reload("lib/**/*.rb")
+require("./lib/employee")
+require("./lib/division")
+require("pg")
+
+get("/") do
+  @employees = Employee.all()
+  erb(:index)
+end
+
+get('/employees') do
+  @employees = Employee.all()
+  erb(:employees)
+end
+
+get('/employees/new') do
+  erb(:employee_form)
+end
+
+post('/employees') do
+  @employees = Employee.all()
+  name = params.fetch('name')
+  @employee = Employee.new({:name => name})
+  @employee.save()
+end
+
+get('/employees/:id') do
+  @employee = Employee.find(params.fetch('id').to_i())
+  erb(:employee)
+end
+
+get('/employees/:id/edit') do
+  @employee = Employee.find(params.fetch('id').to_i())
+  erb(:employee_edit)
+end
+
+patch('/employees/:id') do
+  name = params.fetch('description')
+  @employee = Employee.find(params.fetch('id').to_i())
+  @employee.update({:name => name})
+  @employees = Employee.all()
+  erb(:employees)
+end
